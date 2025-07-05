7c08c8efdfbcb67211a19ad7c55d6b56d075fb2b
InshafRajaaei <inshafrmnaazir@gmail.com>
2025-07-05 15:03:55 +0530

    cart

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -57,11 +57,39 @@ const ShopContextProvider = (props) => {
     //     console.log(cartItems);
     // },[cartItems])
 
+    const updateQuantity = async (itemId,size,quantity) => {
+
+        let cartData = structuredClone(cartItems);
+
+        cartData[itemId][size] = quantity;
+
+        setCartIrems(cartData);
+
+    }
+
+    const getCartAmount = () => {
+        let totalAmount = 0;
+        for(const items in cartItems){
+            let itemInfo = products.find((products)=> products._id === items);
+            for(const item in cartItems[items]){
+                try{
+                    if(cartItems[items][item] > 0){
+                        totalAmount += itemInfo.price * cartItems[items][item];
+                    }
+                } catch (error) {
+
+                }
+            }
+        }
+        return totalAmount;
+    }
+
     const value = {
         products , currency , delivery_fee,
         search,setSearch,showSearch,setShowSearch,
         cartItems,addToCart,
-        getCartCount
+        getCartCount,updateQuantity,
+        getCartAmount
     }
 
     return(
