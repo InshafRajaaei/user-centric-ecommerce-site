a63bb8f52cdbb500bd651f8d2d68154c89f03b03
InshafRajaaei <inshafrmnaazir@gmail.com>
2025-08-03 23:03:13 +0530

    ..

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import {Routes, Route} from 'react-router-dom'
 import Add from './pages/Add'
+import List from './pages/List'
 
 const App = () => {
   return (
@@ -15,7 +16,8 @@ const App = () => {
         <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 to-gray-600 text-base'>
           <Routes>
             <Route path='/add' element={<Add/>}></Route>
-            {/* <Route path='/list' element={}></Route> */}
+            <Route path='/list' element={<List/>}></Route>
+
           </Routes>
 
         </div>
