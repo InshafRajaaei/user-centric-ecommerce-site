7fa3700919de473e2489e72b7db7e9c155eb277f
InshafRajaaei <inshafrmnaazir@gmail.com>
2025-08-03 22:18:59 +0530

    Admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
+import {Routes, Route} from 'react-router-dom'
+import Add from './pages/Add'
 
 const App = () => {
   return (
@@ -10,6 +12,13 @@ const App = () => {
       <hr />
       <div className='flex w-full'>
         <Sidebar></Sidebar>
+        <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 to-gray-600 text-base'>
+          <Routes>
+            <Route path='/add' element={<Add/>}></Route>
+            {/* <Route path='/list' element={}></Route> */}
+          </Routes>
+
+        </div>
       </div>
       </>
     </div>
