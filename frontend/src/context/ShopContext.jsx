24e946612f08d628e1e4e26fb6bd44367f9c53c2
InshafRajaaei <inshafrmnaazir@gmail.com>
2025-08-05 19:20:05 +0530

    update

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -15,6 +15,7 @@ const ShopContextProvider = (props) => {
     const [showSearch,setShowSearch] = useState(false);
     const [cartItems,setCartIrems] = useState({});
     const [products, setProducts] = useState([]);
+    const [token,setToken] = useState('')
     const navigate = useNavigate();
 
     const addToCart = async (itemId,size) => {
@@ -107,17 +108,26 @@ const ShopContextProvider = (props) => {
         }
     }
 
+    useEffect(()=>{
+        getProductsData()
+    },[])
+
+    useEffect(()=>{
+        if(!token && localStorage.getItem('token')){
+            setToken(localStorage.getItem('token'))
+        }
+    },[])
+
     const value = {
         products , currency , delivery_fee,
         search,setSearch,showSearch,setShowSearch,
         cartItems,addToCart,
         getCartCount,updateQuantity,
-        getCartAmount,navigate , backendUrl
+        getCartAmount,navigate , backendUrl,
+        setToken,token
     }
 
-    useEffect(()=>{
-        getProductsData()
-    },[])
+    
 
     return(
         <ShopContext.Provider value={value}>
